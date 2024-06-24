61062766b8487a88646d0809b6165c5083578e33
SathishDornala <164191366+SathishDornala@users.noreply.github.com>
2024-06-24 22:52:45 +0530

    if_else.py

--- if_else.py
+++ if_else.py
@@ -1,48 +1,55 @@
-# num=int(input("Enter the any number to check even or odd:"));
-# if(num%2==0):
-#     print(num,"is Even");
+# checking even number
+num=int(input("Enter the any number to check even or odd:"));
+ if(num%2==0):
+     print(num,"is Even");
     
-    
-# a=int(input("Enter a:"));
-# b=int(input("Enter b:"));
-# c=int(input("Enter c:"));
-# if(a>b and a>c):
-#     print(a,"is bigger");
-# if(b>a and b>c):
-#     print(b,"is bigger");
-# if(c>a and c>b):
-#     print(c,"is bigger");
-    
-# name=input("Enter your name:");
-# age=int(input("Enter your age:"));
-# if(age<18):
-#     print(name,"You are not Eligible to vote");
-# else:
-#     print(name,"You are Eligible to vote");
-    
-# num=int(input("Enter any number:"));
-# if(num%2==0):
-#     print("Even number");
-# else:
-#     print("not a  Even number");
+ #checking bigger among a,b,c   
+ a=int(input("Enter a:"));
+ b=int(input("Enter b:"));
+ c=int(input("Enter c:"));
+ if(a>b and a>c):
+     print(a,"is bigger");
+ if(b>a and b>c):
+     print(b,"is bigger");
+ if(c>a and c>b):
+     print(c,"is bigger");
 
-# num=int(input("Enter any number:"));
-# if(num==5):
-#     print("you entered 5");
-# elif(num==10):
-#      print("you entered 10");
-# elif(num==15):
-#     print("you entered 15");
-# else:
-#     print("you entered non of 5,10,15");
 
-# name=input("Enter your name :");
-# marks=float(input("Enter your marks:"));
-# if(marks>=90 and marks<=100):
-#     print(name,"outstanding performace with",marks);
-# elif(marks<90 and marks>=80):
-#     print(name,"you got A+ Grade with ",marks);
-# elif(marks<80 and marks>=70):
-#     print(name,"you got A Grade with",float);
-# else:
-#     print(name,"better luck next time");
+# checking vote Eligibility 
+ name=input("Enter your name:");
+ age=int(input("Enter your age:"));
+ if(age<18):
+     print(name,"You are not Eligible to vote");
+else:
+     print(name,"You are Eligible to vote");
+
+
+  
+ num=int(input("Enter any number:"));
+ if(num%2==0):
+     print("Even number");
+ else:
+     print("not a  Even number");
+
+
+ num=int(input("Enter any number:"));
+ if(num==5):
+     print("you entered 5");
+ elif(num==10):
+      print("you entered 10");
+ elif(num==15):
+     print("you entered 15");
+ else:
+     print("you entered non of 5,10,15");
+
+
+ name=input("Enter your name :");
+ marks=float(input("Enter your marks:"));
+ if(marks>=90 and marks<=100):
+     print(name,"outstanding performace with",marks);
+ elif(marks<90 and marks>=80):
+     print(name,"you got A+ Grade with ",marks);
+ elif(marks<80 and marks>=70):
+     print(name,"you got A Grade with",float);
+ else:
+     print(name,"better luck next time");
