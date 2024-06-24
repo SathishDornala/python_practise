466b16dbe3a0e218eb77bab97a0d4feece7c8e89
SathishDornala <164191366+SathishDornala@users.noreply.github.com>
2024-06-24 22:43:33 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,26 @@
+a=9;
+b=8;
+# print(a+b,a-b,a*b,a/b,a%b,a//2,a**2);  #Arthmetic operator
+
+c=9;
+d=8;
+# print(a==b,a>=c,c<d,d<=2,a!=b);   #comparision operators
+
+# a+=b;b-=c;c*+d;d/=a;
+# print(a,b,c,d);        #Assignment operators
+
+
+# print(a&b,a and b,a|b,a or b,a^b,~b,a>>b,a<<b); #bitwise operators
+
+# print(a and b,a or b, not b); #logical operator
+
+# x=[1,2,3,4,5,5];      #membership operator
+# print(6 in x);
+# print("6" not in x);
+
+# z=[1,2,4];
+# y=[1,2,4];
+# print(z is y);
+# print(z==y);          #identity operator
+# print(y is not z);
+# print(z!=y); 
